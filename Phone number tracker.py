8b3b0f51206d6d8769716a7a271a3bfc1024c650
Vishesh chintan <vishesh.chintan1206@gmail.com>
2022-12-21 13:55:25 +0530

    Small_change_in_phoneNumber

--- Phone number tracker.py
+++ Phone number tracker.py
@@ -1,10 +1,10 @@
 import phonenumbers
 from phonenumbers import geocoder
 from phonenumbers import carrier
-phone_number_1 = phonenumbers.parse("+919414784307","CH")
+phone_number_1 = phonenumbers.parse("+91123456789","CH")
 result = geocoder.description_for_number(phone_number_1,"en")
 print(result)
 if result=="India":
-    phone_number_1 = phonenumbers.parse("+911234567890","RO")
+    phone_number_1 = phonenumbers.parse("+91123456789","RO")
     print(carrier.name_for_number(phone_number_1,"en"))
 
